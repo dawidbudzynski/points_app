7e9d08e01b5c04f704630fcf77ed71c01158db19
Dawid <budzynski.dawid@gmail.com>
2021-03-03 16:15:38 +0100

    Added User model + tests

--- app/settings.py
+++ app/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -104,3 +105,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+AUTH_USER_MODEL = 'user.User'
